1aab01c8b21779cd034bd0db7f1a3ec520c193da
Aronja <anovicky@thoughtworks.com>
2019-03-31 23:18:48 +0100

    Implements Webserver and react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import App from "./App";
+import './index.html'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () =>(
+    <div>
+        Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!
+    </div>
+)
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
+var path    = require('path');
+var hwp     = require('html-webpack-plugin');
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        filename: 'build.js',
+        path: path.join(__dirname, '/dist')
+    },
+    module:{
+        rules:[{
+            exclude: /node_modules/,
+            test: /\.jsx?$$/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins:[
+        new hwp({template:path.join(__dirname, './src/index.html')})
     ]
-  }
-};
+}
